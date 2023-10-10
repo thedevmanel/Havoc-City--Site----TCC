d921216efab1c71ba3728114363e9aafeebfe8a4
thedevmanel <emanuelzrabello@gmail.com>
2023-10-09 22:25:01 -0300

    Update Date

--- usuario.php
+++ usuario.php
@@ -1,8 +1,4 @@
-<?php
-
-include "php/verificacao_id.php";
-
-?>
+<!-- <?php include "php/verificacao_id.php"; ?> -->
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,11 +6,31 @@ include "php/verificacao_id.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/usuario.css">
     <title> Minha Conta </title>
 </head>
 
 <body>
-    <?php echo $id . $nome . $nickname . $email . $senha; ?>
+    <nav id="menu-h">
+        <div class="logo-jogo-header">
+            <img src="https://fontmeme.com/permalink/230529/06735112d397a33f5f981ecb17a4ad41.png" class="logo-header" />
+        </div>
+
+        <ul>
+            <li><a href="index.html">Home </a></li>
+
+            <li><a href="sobre.html">Sobre</a></li>
+
+            <li><a href="Download.html">Download</a></li>
+
+            <li><a href="tutorial.html">Tutorial</a></li>
+
+            <li><a href="entrar.html">iniciar sessão</a></li>
+        </ul>
+    </nav>
+
+    
 </body>
 
 </html>
