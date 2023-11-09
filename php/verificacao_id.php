a3e08a0026e4212a7698429e20d629d963c6b9fc
thedevmanel <emanuelzrabello@gmail.com>
2023-11-09 15:37:01 -0300

    added error method

--- php/verificacao_id.php
+++ php/verificacao_id.php
@@ -4,16 +4,35 @@ include "conexao.php";
 
 $id = $_GET['id'];
 
+$gatilho = false;
+
 $query_select = "SELECT * FROM `usuario` WHERE `id_user` = '$id'";
 $select = mysqli_query($conn, $query_select);
 
 if (($select) && ($select->num_rows != 0)) {
     while ($rows = mysqli_fetch_array($select)) {
-        $nome = $rows['nome'];
-        $nickname = $rows['nickname'];
-        $email = $rows['email'];
-        $senha = $rows['senha'];
+        $detect_id = $rows['id_user'];
+
+        if ($detect_id ==  $id) {
+            $gatilho = true;
+            $nome = $rows['nome'];
+            $nickname = $rows['nickname'];
+            $email = $rows['email'];
+            $senha = $rows['senha'];
+        }else {
+        }
     }
+}
+
+if ($gatilho ==  false) {
+    mysqli_close($conn);
+
+    echo "<script language='javascript' type='text/javascript'>
+        alert('Infelizmente ocorreu um pequeno problema para acessar sua conta, repita o processo de login');
+        window.location.href='http://localhost//Havoc-City--Site----TCC/entrar.html';
+    </script>";
+
+} else {
 }
 
 mysqli_close($conn);
