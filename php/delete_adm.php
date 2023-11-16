fede533adee05db377c0c289176bd76b986f89eb
thedevmanel <emanuelzrabello@gmail.com>
2023-11-15 21:06:57 -0300

    upgrade admin

--- php/delete_adm.php
+++ php/delete_adm.php
@@ -9,6 +9,7 @@ $delete_query = "DELETE FROM `jogo` WHERE `id_jogo`='$id'";
 if (mysqli_query($conn, $delete_query)) {
     echo "<script>
         alert('O usuario foi deletado');
+        window.location.href='http://localhost//Havoc-City--Site----TCC/adm_gen.php';
     </script>";
 } else {
     echo "Error" . $delete_query, "<br>" . mysqli_error($conn);
