d84401a9f03df34cf1824a03b5657ee0ef5a9725
thedevmanel <emanuelzrabello@gmail.com>
2023-11-19 13:31:08 -0300

    update admin

--- php/update_date_user.php
+++ php/update_date_user.php
@@ -8,15 +8,37 @@ $senha = $_GET['senha'];
 
 include "conexao.php";
 
-$update = "UPDATE `usuario` SET `nome` = '$nome', `nickname` = '$nickname', `email` = '$email', `senha` = '$senha' WHERE `id_user` = '$id'";
+$gatilho = true;
 
-if (mysqli_query($conn, $update)) {
-} else {
-    echo "Error" . $update, "<br>" . mysqli_error($conn);
+$query_select = "SELECT * FROM `usuario` WHERE `email` = '$email' AND `senha` = '$senha'";
+$select = mysqli_query($conn, $query_select);
+
+if (($select) && ($select->num_rows != 0)) {
+    while ($rows = mysqli_fetch_array($select)) {
+        $detect_email = $rows['email'];
+
+        if ($detect_email == $email) {
+            $gatilho = false;
+        } else {
+        }
+    }
 }
+if ($gatilho ==  false) {
+    mysqli_close($conn);
 
-$url = "http://localhost//Havoc-City--Site----TCC/usuario.php?id=" . urlencode($id);
+    echo "<script language='javascript' type='text/javascript'>
+        alert('Já existe esse email registrado');
+        window.location.href='http://localhost//Havoc-City--Site----TCC/entrar.php';
+    </script>";
+} else {
+    $update = "UPDATE `usuario` SET `nome` = '$nome', `nickname` = '$nickname', `email` = '$email', `senha` = '$senha' WHERE `id_user` = '$id'";
 
-header('Location: ' . $url);
+    if (mysqli_query($conn, $update)) {
+    } else {
+        echo "Error" . $update, "<br>" . mysqli_error($conn);
+    }
 
-?>
+    $url = "http://localhost//Havoc-City--Site----TCC/usuario.php?id=" . urlencode($id);
+
+    header('Location: ' . $url);
+}
